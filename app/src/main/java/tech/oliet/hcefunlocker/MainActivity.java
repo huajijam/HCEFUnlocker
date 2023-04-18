df3f706166783f9db20f52696e044ea69de4e474
‭huajijam <strhuaji@gmail.com>
2023-04-19 02:06:26 +0800

    feat: about page,hide launcher icon

--- app/src/main/java/tech/oliet/hcefunlocker/MainActivity.java
+++ app/src/main/java/tech/oliet/hcefunlocker/MainActivity.java
@@ -1,13 +1,21 @@
 package tech.oliet.hcefunlocker;
 
 import android.annotation.SuppressLint;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Process;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,7 +26,7 @@ import java.lang.reflect.Method;
 public class MainActivity extends AppCompatActivity {
     private boolean isHCEFSupported = false;
     private boolean isHCEFUnlocked = false;
-
+    MenuItem hideLauncherIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +43,45 @@ public class MainActivity extends AppCompatActivity {
             text.setText(R.string.support_state_false);
             text.setTextColor(Color.RED);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        hideLauncherIcon = menu.findItem(R.id.hide_launcher_icon);
+        hideLauncherIcon.setChecked(ReadSharedPreferences("hide_icon"));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.hide_launcher_icon:
+                SharedPreferences preferences = getApplicationContext().getSharedPreferences("HCEFUnlocker", android.content.Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                PackageManager hcefunlocker = getPackageManager();
+                ComponentName componentName = new ComponentName(this, "tech.oliet.hcefunlocker.MainActivityLauncher");
+
+                if (hideLauncherIcon.isChecked()) {
+                    hideLauncherIcon.setChecked(false);
+                    hcefunlocker.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+                    Toast.makeText(getApplication(), R.string.hide_desktop_disable, Toast.LENGTH_SHORT).show();
+                    editor.putBoolean("hide_icon", false);
+                } else {
+                    hideLauncherIcon.setChecked(true);
+                    hcefunlocker.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+                    Toast.makeText(getApplication(), R.string.hide_desktop_enable, Toast.LENGTH_SHORT).show();
+                    editor.putBoolean("hide_icon", true);
+                }
+                editor.commit();
+                break;
+            case R.id.about:
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/OLIET2357/HCEFUnlocker"));
+                startActivity(browserIntent);
+                break;
+        }
+        return true;
     }
 
     @Override
@@ -89,5 +136,10 @@ public class MainActivity extends AppCompatActivity {
         } else {
             return (boolean) HiddenApiBypass.invoke(clazz, null, "isValidSystemCode", systemCode);
         }
+    }
+
+    private boolean ReadSharedPreferences(String key){
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("HCEFUnlocker", android.content.Context.MODE_PRIVATE);
+        return preferences.getBoolean(key, false);
     }
 }
